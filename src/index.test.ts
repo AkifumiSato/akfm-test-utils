56a9e2c3174321c48ca9a611846a22ef8109f6e7
佐藤昭文 <akfm.sato@gmail.com>
2025-03-09 11:51:51 +0900

    async support

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ import { step } from ".";
 describe("`step()`", () => {
   describe("with arrange specified", () => {
     test(
-      "can perform simple calculations",
+      "Ability to perform simple calculations.",
       step({
         arrange: () => ({
           a: 1,
@@ -20,11 +20,11 @@ describe("`step()`", () => {
     );
 
     test(
-      "can pass complex objects around",
+      "Ability to pass complex objects around.",
       step({
         arrange: () => ({
           user: {
-            name: "テストユーザー",
+            name: "Test User",
             age: 30,
           },
           permissions: ["read", "write"],
@@ -34,7 +34,29 @@ describe("`step()`", () => {
           ...arrange,
         }),
         assert: ({ arrange, act }) => {
-          expect(act.displayName).toBe("テストユーザー (30)");
+          expect(act.displayName).toBe("Test User (30)");
+          expect(act.user).toEqual(arrange.user);
+          expect(act.permissions).toEqual(arrange.permissions);
+        },
+      }),
+    );
+
+    test(
+      "Supports asynchronous arrange, act, assert.",
+      step({
+        arrange: async () => ({
+          user: {
+            name: "Test User",
+            age: 30,
+          },
+          permissions: ["read", "write"],
+        }),
+        act: async ({ arrange }) => ({
+          displayName: `${arrange.user.name} (${arrange.user.age})`,
+          ...arrange,
+        }),
+        assert: async ({ arrange, act }) => {
+          expect(act.displayName).toBe("Test User (30)");
           expect(act.user).toEqual(arrange.user);
           expect(act.permissions).toEqual(arrange.permissions);
         },
@@ -44,10 +66,20 @@ describe("`step()`", () => {
 
   describe("arrange omitted", () => {
     test(
-      "can perform simple calculations",
+      "Ability to perform simple calculations.",
       step({
         act: () => 1 + 2,
         assert: ({ act }) => {
+          expect(act).toBe(3);
+        },
+      }),
+    );
+
+    test(
+      "Supports asynchronous act.",
+      step({
+        act: async () => 1 + 2,
+        assert: async ({ act }) => {
           expect(act).toBe(3);
         },
       }),
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 type DefinitionWithArrange<Arrange, Act> = {
-  arrange: () => Arrange;
-  act: (context: { arrange: Arrange }) => Act;
-  assert: (context: { arrange: Arrange; act: Act }) => void;
+  arrange: () => Arrange | Promise<Arrange>;
+  act: (context: { arrange: Arrange }) => Act | Promise<Act>;
+  assert: (context: { arrange: Arrange; act: Act }) => void | Promise<void>;
 };
 
 type DefinitionWithoutArrange<Act> = {
-  act: () => Act;
-  assert: (context: { act: Act }) => void;
+  act: () => Act | Promise<Act>;
+  assert: (context: { act: Act }) => void | Promise<void>;
 };
 
 export function step<Arrange, Act>(
@@ -20,21 +20,24 @@ export function step<Arrange, Act>(
     | DefinitionWithArrange<Arrange, Act>
     | DefinitionWithoutArrange<Act>,
 ): () => void {
-  return () => {
+  return async () => {
     // narrowing type
     if ("arrange" in definition) {
-      const arrange = definition.arrange();
-      const act = definition.act({ arrange });
-      definition.assert({ arrange, act });
+      const arrangeResult = definition.arrange();
+      const arrange =
+        arrangeResult instanceof Promise ? await arrangeResult : arrangeResult;
+      const actResult = definition.act({ arrange });
+      const act = actResult instanceof Promise ? await actResult : actResult;
+      await definition.assert({ arrange, act });
     } else {
-      const act = definition.act();
-      definition.assert({ act });
+      const actResult = definition.act();
+      const act = actResult instanceof Promise ? await actResult : actResult;
+      await definition.assert({ act });
     }
   };
 }
 
 /**
  * TODO
- * - [ ] 非同期対応
  * - [ ] Arrangeの共通化を促す（ArrangeFactory?）
  */
